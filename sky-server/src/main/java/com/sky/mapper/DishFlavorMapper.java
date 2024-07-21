421bb2b9f5b5c04eedd7e8342f455f9dd20aeb8b
Torosamy <1364596766@qq.com>
2024-07-21 21:13:37 +0800

    save dish

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,16 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味数量
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+
+}
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -2,15 +2,26 @@ package com.sky.service.impl;
 
 import com.sky.dto.DishDTO;
 import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@Slf4j
 public class DishServiceImpl implements DishService {
     @Autowired
     private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
     /**
      * 新增菜品和对应的口味
      * @param dishDTO
@@ -21,6 +32,15 @@ public class DishServiceImpl implements DishService {
         BeanUtils.copyProperties(dishDTO,dish);
         //向菜品表添加数据
         dishMapper.insert(dish);
-        //向口味表插入n条数据
+        Long dishId = dish.getId();
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && flavors.size() > 0) {
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            //向口味表插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
     }
 }
